45f8fcddc2e74b5948eb06e6d133f963874adcec
DrewDrizzle <aramouni8@gmail.com>
2019-02-05 18:06:44 -0500

    added more

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,25 @@
 // Your apiRoutes.js file should contain two routes:
-// A GET route with the url /api/friends. 
-// This will be used to display a JSON of all possible friends. 
-// A POST routes /api/friends. This will be used to handle incoming survey results. 
-// This route will also be used to handle the compatibility logic.
+    // A GET route with the url /api/friends. 
+    // This will be used to display a JSON of all possible friends. 
+    
+    // A POST routes /api/friends. This will be used to handle incoming survey results. 
+    // This route will also be used to handle the compatibility logic.
+
+const friends = express(); 
+
+var PORT = 3000;
+
+friends.GET('/api/friends', (req, res, next) => { //GET means this will display all POSSIBLE FRIENDS on a this page
+        
+});
+friends.POST('/api/friends', (req, res, next) => { //POST IS FOR HANDLING INCOMING SURVEY RESULTS
+
+
+} ) 
+
+// Listener
+// ===========================================================
+friends.listen(PORT, function() {
+    console.log("friends listening on PORT " + PORT);
+});
+  
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,18 @@
 // Your htmlRoutes.js file should include two routes:
-// A GET Route to /survey which should display the survey page. 
-// A default, catch-all route 
-// that leads to home.html which displays the home page
+    // A GET Route to /survey which should display the survey page. 
+    
+    // A default, catch-all route 
+    // that leads to home.html which displays the home page
+
+const PORT = 3000;
+
+const homeFF = express(); //homeFF means FriendFinder home page on the page
+homeFF.get('/survey', (req, res, next) => {
+    
+});
+
+// Listener
+// ===========================================================
+homeFF.listen(PORT, function() {
+    console.log("homeFF listening on PORT " + PORT);
+});
